dc78a3292c52dfa2e1312cc6f027c32ffd4ae42c
Dodzi Keglo <stephanekeglo@gmail.com>
2025-01-26 22:44:49 +0000

    Implement removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...args) {
+  for (const value of args) {
+    while (array.indexOf(value) !== -1) {
+      array.splice(array.indexOf(value), 1)
+    }
+  }
+  return array
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
