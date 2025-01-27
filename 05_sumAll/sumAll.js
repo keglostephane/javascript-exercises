24a12c044fd94b59632e26da8c393ef373d7e7f6
Dodzi Keglo <stephanekeglo@gmail.com>
2025-01-27 12:29:05 +0000

    Implement sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
-};
+const sumAll = function (number1, number2) {
+  let sum = 0
+  if (Number.isInteger(number1) && number1 > 0 &&
+		Number.isInteger(number2) && number2 > 0) {
+    if (number1 < number2) {
+      while (number1 <= number2) {
+        sum += number1
+        number1++
+      }
+    } else if (number1 > number2) {
+      while (number1 >= number2) {
+        sum += number1
+        number1--
+      }
+    } else return number1
+    return sum
+  } else return 'ERROR'
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
