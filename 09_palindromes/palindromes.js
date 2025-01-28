3f7136c660e19f7733fe0623af10e2d6d9bdbed4
Dodzi Keglo <stephanekeglo@gmail.com>
2025-01-28 00:15:34 +0000

    Implement palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+  const regexp = /\w/g		// match word characters
+  const match = string.match(regexp).join('').toLowerCase()
+  const reversed = string.match(regexp).reverse().join('').toLowerCase()
+  return match === reversed
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
