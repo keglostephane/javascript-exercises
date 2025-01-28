1bf98c5e2e5ac4619e11da8db257820fbec83ce1
Dodzi Keglo <stephanekeglo@gmail.com>
2025-01-28 12:22:31 +0000

    Implement fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (rank) {
+  const num = parseInt(rank, 10)
+  if (num === NaN) return 'OOPS'
+  if (num < 0) return 'OOPS'
+  if (num === 0) return 0
+  if (num === 1) return 1
+  if (num === 2) return 1
+  return fibonacci(num - 2) + fibonacci(num - 1)
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
