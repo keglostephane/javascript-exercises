691dac584bbc555e53c41c067c249f5320868345
Dodzi Keglo <stephanekeglo@gmail.com>
2025-01-30 10:38:55 +0000

    Implement findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldest, person) => {
+    const personAge = person.yearOfDeath
+      ? person.yearOfDeath - person.yearOfBirth
+      : (new Date()).getFullYear - person.yearOfBirth
+    const oldestAge = oldest.yearOfDeath
+      ? oldest.yearOfDeath - oldest.yearOfBirth
+      : (new Date()).getFullYear() - oldest.yearOfBirth
 
-};
+    return personAge > oldestAge ? person : oldest
+  })
+}
 
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = findTheOldest
